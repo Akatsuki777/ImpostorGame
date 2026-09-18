a1b407f3901bb15fa32fec94eb852abfae5e0e0a
Aju Joy <ajujoy777@gmail.com>
2026-09-18 14:17:25 -0700

    Fixes shapes exceeding the container sizes]

--- frontend/src/components/ui/ShapesHeader.tsx
+++ frontend/src/components/ui/ShapesHeader.tsx
@@ -1,65 +1,121 @@
-import {motion} from "motion/react"
-
 export default function ShapesHeader() {
 
-    const svgStyle = 'col-start-1 row-start-1 drop-shadow-[4px_4px_0px_#EBEBEB]';
+    const svgStyle = 'block w-full h-full';
 
     return (
         <div className={`w-full h-fit flex justify-between`}>
             <div 
-                className={`my-4 w-20 h-20 -rotate-15`}>
-                <svg
-                    className={`${svgStyle}`}
-                    fill="#FA448C"
-                    width="80px"
-                    height="80px"
-                    viewBox="0 0 50 50"
-                    version="1.2"
-                    baseProfile="tiny"
-                    xmlns="http://www.w3.org/2000/svg"
-                    overflow="inherit"
+                className={`relative isolate my-4 w-20 h-20 -rotate-15`}>
+                <div
+                    className={`absolute inset-0 right-1 bottom-1`}
                 >
-                <path d="M1 1h48v48h-48z" />
-                </svg>
-                
+                    <svg
+                        className={`${svgStyle}`}
+                        fill="#FA448C"
+                        viewBox="0 0 50 50"
+                        version="1.2"
+                        baseProfile="tiny"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <path d="M1 1h48v48h-48z" />
+                    </svg>
+                </div>
+                <div
+                    className={`absolute inset-0 top-1 left-1 -z-1`}
+                >
+                    <svg
+                        className={`${svgStyle}`}
+                        fill="#EBEBEB"
+                        viewBox="0 0 50 50"
+                        version="1.2"
+                        baseProfile="tiny"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <path d="M1 1h48v48h-48z" />
+                    </svg>
+                </div>
             </div>
-            <div className={`my-0.5 origin-center w-25 h-25 rotate-75`}>
-                <svg
-                    className={`${svgStyle}`}
-                    width="100px"
-                    height="100px"
-                    fill="#43B5A0"
-                    viewBox="0 0 490 490"
+            <div className={`relative isolate my-0.5 origin-center w-25 h-25 rotate-75`}>
+                <div
+                    className={`absolute inset-0 right-1 bottom-1`}
                 >
-                <polygon points="245,456.701 490,33.299 0,33.299 " />
-                </svg>
+                    <svg
+                        className={`${svgStyle}`}
+                        fill="#43B5A0"
+                        viewBox="0 0 490 490"
+                    >
+                    <polygon points="245,456.701 490,33.299 0,33.299 " />
+                    </svg>
+                </div>
+                <div
+                    className={`absolute inset-0 top-1 left-1 -z-1`}
+                >
+                    <svg
+                        className={`${svgStyle}`}
+                        fill="#EBEBEB"
+                        viewBox="0 0 490 490"
+                    >
+                    <polygon points="245,456.701 490,33.299 0,33.299 " />
+                    </svg>
+                </div>
             </div>
-            <div className={`origin-center w-28 h-28 rotate-20`}>
-                <svg
-                    className={`${svgStyle}`}
-                    width="110px"
-                    height="110px"
-                    fill="#491D88"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
+            <div className={`relative isolate origin-center w-25 h-25 rotate-20`}>
+                <div
+                    className={`absolute inset-0 right-1 bottom-1`}
                 >
-                <path d="M18.847 22H5.215L.397 9.544 12 1l11.6 8.543z" />
-                </svg>
+                    <svg
+                        className={`${svgStyle}`}
+                        fill="#491D88"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <path d="M18.847 22H5.215L.397 9.544 12 1l11.6 8.543z" />
+                    </svg>
+                </div>
+                <div
+                    className={`absolute inset-0 top-1 left-1 -z-1`}
+                >
+                    <svg
+                        className={`${svgStyle}`}
+                        fill="#EBEBEB"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <path d="M18.847 22H5.215L.397 9.544 12 1l11.6 8.543z" />
+                    </svg>
+                </div>
             </div>
-            <div className={`origin-center`} >
-                <svg
-                    className={`${svgStyle}`}
-                    width="110px"
-                    height="110px"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#FEC859"
-                    version="1.1"
-                    viewBox="0 0 594.39 595.28"
+            <div className={`relative isolate w-25 h-25 origin-center`} >
+                <div
+                    className={`absolute inset-0 right-1 bottom-1`}
                 >
-                <path
-                    d="M234.39,62.8C234.39,28.14,262.53,0,297.19,0s62.8,28.14,62.8,62.8c17.33-30.02,55.77-40.32,85.79-22.99,30.02,17.33,40.32,55.77,22.99,85.79,30.02-17.33,68.46-7.03,85.79,22.99,17.33,30.02,7.03,68.46-22.99,85.79,34.66,0,62.8,28.14,62.8,62.8s-28.14,62.81-62.8,62.81c30.02,17.33,40.32,55.77,22.99,85.79-17.33,30.02-55.77,40.32-85.79,22.99,17.33,30.02,7.03,68.46-22.99,85.79-30.02,17.33-68.46,7.03-85.79-22.99,0,34.66-28.14,62.8-62.8,62.8s-62.81-28.14-62.81-62.8c-17.33,30.02-55.77,40.32-85.79,22.99-30.02-17.33-40.32-55.77-22.99-85.79-30.02,17.33-68.46,7.03-85.79-22.99-17.33-30.02-7.03-68.46,22.99-85.79-34.66,0-62.8-28.14-62.8-62.81s28.14-62.8,62.8-62.8c-30.02-17.33-40.32-55.77-22.99-85.79,17.33-30.02,55.77-40.32,85.79-22.99-17.33-30.02-7.03-68.46,22.99-85.79,30.02-17.33,68.46-7.03,85.79,22.99Z"
-                />
-                </svg>
+                    <svg
+                        className={`${svgStyle}`}
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="#FEC859"
+                        version="1.1"
+                        viewBox="0 0 594.39 595.28"
+                    >
+                    <path
+                        d="M234.39,62.8C234.39,28.14,262.53,0,297.19,0s62.8,28.14,62.8,62.8c17.33-30.02,55.77-40.32,85.79-22.99,30.02,17.33,40.32,55.77,22.99,85.79,30.02-17.33,68.46-7.03,85.79,22.99,17.33,30.02,7.03,68.46-22.99,85.79,34.66,0,62.8,28.14,62.8,62.8s-28.14,62.81-62.8,62.81c30.02,17.33,40.32,55.77,22.99,85.79-17.33,30.02-55.77,40.32-85.79,22.99,17.33,30.02,7.03,68.46-22.99,85.79-30.02,17.33-68.46,7.03-85.79-22.99,0,34.66-28.14,62.8-62.8,62.8s-62.81-28.14-62.81-62.8c-17.33,30.02-55.77,40.32-85.79,22.99-30.02-17.33-40.32-55.77-22.99-85.79-30.02,17.33-68.46,7.03-85.79-22.99-17.33-30.02-7.03-68.46,22.99-85.79-34.66,0-62.8-28.14-62.8-62.81s28.14-62.8,62.8-62.8c-30.02-17.33-40.32-55.77-22.99-85.79,17.33-30.02,55.77-40.32,85.79-22.99-17.33-30.02-7.03-68.46,22.99-85.79,30.02-17.33,68.46-7.03,85.79,22.99Z"
+                    />
+                    </svg>
+                </div>
+                <div
+                    className={`absolute inset-0 top-1 left-1 -z-1`}
+                >
+                    <svg
+                        className={`${svgStyle}`}
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="#EBEBEB"
+                        version="1.1"
+                        viewBox="0 0 594.39 595.28"
+                    >
+                    <path
+                        d="M234.39,62.8C234.39,28.14,262.53,0,297.19,0s62.8,28.14,62.8,62.8c17.33-30.02,55.77-40.32,85.79-22.99,30.02,17.33,40.32,55.77,22.99,85.79,30.02-17.33,68.46-7.03,85.79,22.99,17.33,30.02,7.03,68.46-22.99,85.79,34.66,0,62.8,28.14,62.8,62.8s-28.14,62.81-62.8,62.81c30.02,17.33,40.32,55.77,22.99,85.79-17.33,30.02-55.77,40.32-85.79,22.99,17.33,30.02,7.03,68.46-22.99,85.79-30.02,17.33-68.46,7.03-85.79-22.99,0,34.66-28.14,62.8-62.8,62.8s-62.81-28.14-62.81-62.8c-17.33,30.02-55.77,40.32-85.79,22.99-30.02-17.33-40.32-55.77-22.99-85.79-30.02,17.33-68.46,7.03-85.79-22.99-17.33-30.02-7.03-68.46,22.99-85.79-34.66,0-62.8-28.14-62.8-62.81s28.14-62.8,62.8-62.8c-30.02-17.33-40.32-55.77-22.99-85.79,17.33-30.02,55.77-40.32,85.79-22.99-17.33-30.02-7.03-68.46,22.99-85.79,30.02-17.33,68.46-7.03,85.79,22.99Z"
+                    />
+                    </svg>
+                </div>
             </div>
         </div>
     );
